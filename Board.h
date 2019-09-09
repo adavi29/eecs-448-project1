a1acd6cdb0df0476bdf62577e25c47314b43f88e
Abigail Davidow <lenexapaws@gmail.com>
2019-09-09 14:32:16 -0500

    Added isHit method

--- Board.h
+++ Board.h
@@ -4,7 +4,7 @@
 using namespace std;
 Class Board{
 private:
-  String m_board[][];
+  String** m_board;
 
 public:
   Board();
@@ -12,4 +12,5 @@ public:
   String getEntryAtPosition();
   void setEntryAtPosition(String entry, int column, int row);
   void printBoard();
+  bool isHit(int column, int row);
 }
