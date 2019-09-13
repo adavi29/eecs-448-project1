323307a837b74f1b3042cc0ae0329be471f1a821
Jacob <Jacob>
2019-09-13 14:05:25 -0500

    Added Ships.cpp and Ships.h

---
+++ Ships.h
@@ -0,0 +1,24 @@
+#ifndef SHIPS_H
+#define SHIPS_H
+
+#include "Board.h"
+#include "Ships.cpp"
+
+using namespace std;
+
+class Ships
+{
+
+public:
+
+
+
+private:
+	  int m_ship1=0;
+	  int m_ship2=0;
+	  int m_ship3=0;
+	  int m_ship4=0;
+	  int m_ship5=0;
+};
+
+#endif
