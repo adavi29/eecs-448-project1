f63661eb90566f34c797fecb422d3ee98e7cf218
Abigail Davidow <lenexapaws@gmail.com>
2019-09-16 12:39:13 -0500

    Copied Ships files from master for use in gameClass branch and fixed  some spacing

--- Ships.cpp
+++ Ships.cpp
@@ -1,3 +1,82 @@
 /*
 @file Ships.cpp
 */
+#include "Ships.h"
+
+Ship(int numShips){
+	m_ship1=0;//initialize ship variables in case they are not assigned a value below
+	m_ship2=0;
+	m_ship3=0;
+	m_ship4=0;
+	m_ship5=0;
+
+	if(numShips <6) {
+		if(numShips>0) {
+			m_ship1 = 1;
+		}
+		if(numShips>1) {
+			m_ship2 = 2;
+		}
+		if(numShips>2) {
+			m_ship3 = 3;
+		}
+		if(numShips>3) {
+			m_ship4 = 4
+		}
+		if(numShips>4){
+			m_ship5 = 5;
+		}
+	}
+}
+
+void decreaseSize(int ship) {
+	if(isSunk(ship) != true) {
+		  if(ship == 1) {
+		    m_ship1--;
+		  }
+			if(ship == 2) {
+				m_ship2--;
+			}
+			if(ship == 3) {
+				m_ship3--;
+			}
+			if(ship == 4) {
+				m_ship4--;
+			}
+			if(ship == 5) {
+				m_ship5--;
+			}
+
+			if(isSunk(ship)){
+				announce(ship);
+			}
+	} else {
+     throw(std::runtime_error("cannot decrease length; ship is already sunk");
+	}
+}
+
+bool isSunk(int ship){
+		   if(ship ==1){
+		     return (m_ship1 <= 0);
+		   }
+
+			 if(ship ==2){
+				 return (m_ship2 <= 0);
+			 }
+
+			 if(ship ==3){
+				 return(m_ship3 <= 0);
+			 }
+
+			 if(ship ==4){
+				 return(m_ship4 <= 0);
+			 }
+
+			 if(ship ==5){
+				 return(m_ship5 <= 0);
+			 }
+}
+
+void announce(int ship){
+	cout << "You've sunk ship "<<ship<<" !\n";
+}
--- Ships.h
+++ Ships.h
@@ -1,3 +1,29 @@
 /*
 @file Ships.h
 */
+#ifndef SHIPS_H
+#define SHIPS_H
+
+#include <stdexcept>
+
+using namespace std;
+
+class Ships
+{
+
+public:
+
+	Ship(int numShips);
+	void decreaseSize(int ship);
+	bool isSunk(int ship);
+	void announce(int ship);
+
+private:
+	  int m_ship1=-1;
+	  int m_ship2=-1;
+	  int m_ship3=-1;
+	  int m_ship4=-1;
+	  int m_ship5=-1;
+};
+
+#endif
