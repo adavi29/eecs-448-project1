74a9f8551b6207bd08e06c56801025510f675d17
flobbus <43078954+flobbus@users.noreply.github.com>
2019-09-20 16:25:56 -0500

    Added documentation: pre, post, param, return

--- Ships.h
+++ Ships.h
@@ -1,8 +1,8 @@
 /**
  *   @author Runtime Terrors (Abby Davidow, Anissa Khan, Grant Schnettgoecke, Jacob Swearingen, Chongzhi Gao)
- *   @date
- *   @file
- *   @brief 
+ *   @date 2019 09 20
+ *   @file Ships.h
+ *   @brief Creates the "fleet" for the game and keeps track of each ships "size", or remaining health
  */
 #ifndef SHIPS_H
 #define SHIPS_H
@@ -16,38 +16,38 @@ class Ships
 
 public:
     /**
-     * @pre
-     * @post
-     * @param
-     * @return
+     * @pre none
+     * @post initializes ships 
+     * @param numShips
+     * @return none
      */
 	Ships(int numShips);
     /**
-     * @pre
-     * @post
-     * @param
-     * @return
+     * @pre Ships(int numShips) must have been called
+     * @post decreases "size" which is essentially the health of a ship
+     * @param int ship is the number of the ship, equivalent to its original size
+     * @return none
      */
 	void decreaseSize(int ship);
     /**
-     * @pre
-     * @post
-     * @param
-     * @return
+     * @pre Ships(int numShips) must have been called
+     * @post returns true if size of given ship is 0; also couts a sunk message
+     * @param int ship is the number of the ship, equivalent to its original size
+     * @return true if size is 0, false if size is not 0
      */
 	bool isSunk(int ship);
     /**
-     * @pre
-     * @post
-     * @param
-     * @return
+     * @pre Ships(int numShips) must have been called
+     * @post returns true if all ships are sunk and false otherwise
+     * @param none
+     * @return true if size of all ships >= 0, false otherwise
      */
 	bool allSunk();
     /**
-     * @pre
-     * @post
-     * @param
-     * @return
+     * @pre Ships(int numShips)must have been called
+     * @post couts to terminal when a ship has been sunk, and which ship it was
+     * @param int ship is the number of the ship, equivalent to its original size
+     * @return none
      */
 	void announce(int ship);
 
