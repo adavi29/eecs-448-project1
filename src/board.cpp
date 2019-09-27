5a1c9eda7154262e94541f5231a6a1b6ab78fbcd
Zach Pearson <zach@zjp.codes>
2019-09-27 11:27:00 -0500

    Fix whitespace errors in board.cpp

--- src/board.cpp
+++ src/board.cpp
@@ -5,56 +5,57 @@
  *   @brief prints out game board and edits board entries
  */
 
-#include "Board.h"
-Board::Board(){
-  m_board = new string*[8];
-  for(int i=0;i<8;i++){
-    m_board[i] = new string[8];
-    for(int j=0;j<8;j++){
-      m_board[i][j] = " ";
-    }
-  }
+#include "board.h"
+
+Board::Board() {
+	m_board = new string*[8];
+	for(int i = 0; i < 8; i++) {
+		m_board[i] = new string[8];
+		for(int j = 0; j < 8; j++) {
+			m_board[i][j] = " ";
+		}
+	}
 }
 
-Board::~Board(){
-  for(int i=0;i<8;i++){
-    delete[] m_board[i];
-  }
-  delete[] m_board;
+Board::~Board() {
+	for(int i = 0; i < 8; i++) {
+		delete[] m_board[i];
+	}
+	delete[] m_board;
 }
 
-string Board::getEntryAtPosition(int column, int row){
-  return(m_board[row][column]);
+string Board::getEntryAtPosition(int column, int row) {
+	return(m_board[row][column]);
 }
 
-void Board::setEntryAtPosition(string entry, int column, int row){
-  m_board[row][column] = entry;
+void Board::setEntryAtPosition(string entry, int column, int row) {
+	m_board[row][column] = entry;
 }
 
-void Board::printBoard(){
-  int character = 0;
-  char letter = 'A';
-  for(int i=0;i<8;i++){
-    character = int(letter);
-    cout<<"     "<<letter;
-    character++;
-    letter = char(character);
-  }
-  cout<<"\n";
-  for(int i=0;i<8;i++){
-    cout<<"  -------------------------------------------------\n";
-    cout<<"  |     |     |     |     |     |     |     |     |\n";
-    for(int j=0;j<8;j++){
-      if(j==0){
-        cout<<i+1<<" ";
-      }
-      cout<<"|  "<<m_board[i][j]<<"  ";
-      if(j==7){
-        cout<<"|\n";
-      }
-    }
-    if(i==7){
-      cout<<"  -------------------------------------------------\n\n";
-    }
-  }
+void Board::printBoard() {
+	int character = 0;
+	char letter = 'A';
+	for(int i = 0; i < 8; i++) {
+		character = int(letter);
+		std::cout << "     " << letter;
+		character++;
+		letter = (char)character;
+	}
+	std::cout << "\n";
+	for(int i = 0; i < 8; i++) {
+		std::cout << "  -------------------------------------------------\n";
+		std::cout << "  |     |     |     |     |     |     |     |     |\n";
+		for(int j = 0; j < 8; j++) {
+			if(j == 0) {
+				std::cout << i+1 <<" ";
+			}
+			std::cout << "|  " << m_board[i][j] << "  ";
+			if(j == 7) {
+				std::cout<<"|\n";
+			}
+		}
+		if(i == 7) {
+			std::cout << "  -------------------------------------------------\n\n";
+		}
+	}
 }
