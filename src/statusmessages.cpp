5d34608d234c04b947d18f70de416022a80f936b
Zach Pearson <zach@zjp.codes>
2019-09-29 13:39:48 -0500

    Add class to generate all status messages to clean up pollution in game.cpp

---
+++ src/statusmessages.cpp
@@ -0,0 +1,51 @@
+// -*- C++ -*-
+/**
+* @author Zach Pearson
+* @file   statusmessages.cpp
+* @date   29 September 2019
+* @brief
+*/
+
+#include <iostream>
+
+#include "statusmessages.h"
+
+void StatusMessages::AskNumShips(){
+
+}
+
+void StatusMessages::ErrorNumShips(){
+
+}
+
+void StatusMessages::ErrorInvalidRow(){
+
+}
+
+void StatusMessages::ErrorInvalidCol(){
+
+}
+
+void StatusMessages::ShipAlreadyThere(){
+
+}
+
+void StatusMessages::ShipInTheWay(){
+
+}
+
+void StatusMessages::AlreadyShotThere(){
+
+}
+
+void StatusMessages::ConfirmHit(){
+
+}
+
+void StatusMessages::ConfirmMiss(){
+
+}
+
+void StatusMessages::NextPlayer(){
+
+}
